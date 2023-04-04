7dfc3770511e53ac1f61b784dd0f5f4537355982
is2win <is2win@yandex.ru>
2023-04-04 06:53:35 +0300

    урок 5 - обработка запросов в Django, Middlewares

---
+++ mysite/requestdataapp/views.py
@@ -0,0 +1,28 @@
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+# Create your views here.
+def process_get_view(request: HttpRequest) -> HttpResponse:
+    a = request.GET.get('a', '')
+    b = request.GET.get('b', '')
+    result = a + b
+    context = {
+        'a': a,
+        'b': b,
+        'result': result,
+    }
+    return render(request, 'requestdataapp/request-query-params.html', context=context)
+
+
+def user_form(request: HttpRequest) -> HttpResponse:
+    return render(request, 'requestdataapp/user-bio-form.html')
+
+
+def handle_file_upload(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST" and request.FILES.get("myfile"):
+        my_file = request.FILES["myfile"]
+        fs = FileSystemStorage()
+        file_name = fs.save(my_file.name, my_file)
+        print("saved file:", file_name)
+    return render(request, 'requestdataapp/file-upload.html')
